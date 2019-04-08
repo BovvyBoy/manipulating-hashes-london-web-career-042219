2da875fd62c3f3eaa92a7b37186c015c206afc77
Daniel Bovington <d.bovington@gmail.com>
2019-04-08 12:30:24 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -21,8 +21,10 @@ def first_challenge
 #      end
 #    end
 #  end
+#end
+contacts["Freddy Mercury"]
+:favorite_icecream_flavors == strawberry.delete_if
 end
-
 
   #remember to return your newly altered contacts hash!
   contacts
